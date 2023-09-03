46b12c9239495ffb33e89d6e3f99cb6b850dd975
Weiglas Ribeiro <weiglasribeiro@gmail.com>
2023-09-03 15:14:54 -0300

    feat: add ApiLogger

---
+++ api/logger_api.py
@@ -0,0 +1,33 @@
+import logging
+
+import os
+
+
+class ApiLogger(logging.Logger):
+    """Custom logger for API application"""
+
+    LOG_LEVELS = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+
+    def __init__(self, name='root'):
+        """
+        Initialize the logger
+        :param name: name of the logger
+        """
+        super().__init__(name)
+
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
+        
+        self.addHandler(handler)
+        self.setLevel(self._get_log_level())
+
+    def _get_log_level(self):
+        level = os.getenv('LOG_LEVEL', 'info').lower()
+        return self.LOG_LEVELS.get(level, logging.INFO)
